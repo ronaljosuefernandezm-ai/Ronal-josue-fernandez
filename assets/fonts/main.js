8193543b521af8eb38b2a46808e1de2b47491a8d
Ronal josue fernandez <ronaljosuefernandezm@gmail.com>
2026-04-04 17:25:18 -0500

    primer commit

---
+++ assets/fonts/main.js
@@ -0,0 +1,5 @@
+
+//seleccionar los elementos del dow
+const contenedorpregunta = document.querySelector('#contenedor-pregunta');
+
+contenedorpregunta.innerHTML = '<h2 clase="pregunta">selecciona el tema:</h2>'
